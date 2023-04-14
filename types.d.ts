1b2de26082ad672896c740f1131950b9d1baff5f
Andrew Boktor <aboktor@microsoft.com>
2023-04-14 15:30:42 -0700

    Adding option to set fetchPriority on script tags

--- types.d.ts
+++ types.d.ts
@@ -5942,6 +5942,11 @@ declare interface JavascriptParserOptions {
 	createRequire?: string | boolean;
 
 	/**
+	 * Specifies global fetchPriority for dynamic import.
+	 */
+	dynamicImportFetchPriority?: boolean | "auto" | "low" | "high";
+
+	/**
 	 * Specifies global mode for dynamic import.
 	 */
 	dynamicImportMode?: "weak" | "eager" | "lazy" | "lazy-once";
@@ -9813,6 +9818,7 @@ declare interface ProvidesObject {
 declare interface RawChunkGroupOptions {
 	preloadOrder?: number;
 	prefetchOrder?: number;
+	fetchPriority?: string;
 }
 type RawLoaderDefinition<
 	OptionsType = {},
